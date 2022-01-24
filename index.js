fc615ab859c12d4b94236f9fa359609d42728d95
Arief D <arief360x@gmail.com>
2022-01-24 16:40:47 +0800

    change js files

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var today = new Date();
+var htmld = document.getElementById("day");
+var htmlm = document.getElementById("month");
+var htmly = document.getElementById("year");
+
+let months = ['Jan', 'Feb', 'March', 'April', 'May'];
+
 var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
+var mm = today.getMonth()
 var yy = today.getFullYear();
 
+
+
 function changeColor(){
-    
+    if(htmld == dd && true){
+    }
+    console.log(months[mm]);
 }
