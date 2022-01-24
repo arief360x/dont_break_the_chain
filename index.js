23c29339cea9e6840fd7bfe16cb32010f870ff50
Antonio Vissoni <antoniovissoni@MacBook-Pro.local>
2022-01-24 17:40:53 +0800

    Added setFirstDay function for calendar.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yy = today.getFullYear();
 
-function changeColor(){
-    
+setFirstDay(7);
+
+// index can be any number from 1 to 7
+// function sets the start of the calendar accordingly
+
+function setFirstDay(index){
+    document.getElementById("calendar--first-day").style.gridColumnStart = index;
+    console.log("First day is " + "works");
 }
+
